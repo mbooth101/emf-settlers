273d38930f6403560f0fb797c3d62b321e111de5
Mat Booth <mat.booth@gmail.com>
2026-04-15 10:14:13 +0100

    Add Tildagon app boilerplate

---
+++ app.py
@@ -0,0 +1,28 @@
+"""
+Copyright (c) 2018-2026 Mat Booth.
+
+This file is part of the Settlers of EMF app for the Tildagon
+(see https://github.com/mbooth101/emf-settlers).
+
+License: MIT
+"""
+
+import math
+import os
+import random
+
+import app
+
+
+class Settlers(app.App):
+
+    def __init__(self):
+        pass
+
+    def update(self, delta):
+        pass
+
+    def draw(self, ctx):
+        pass
+
+__app_export__ = Settlers
